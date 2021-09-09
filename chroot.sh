460e2e6f874e3e0e7a6a0c0e737803f43f8c9492
root <root@jarvis>
2021-09-10 00:45:48 +0200

    Added some packages, changed core count

--- chroot.sh
+++ chroot.sh
@@ -6,6 +6,6 @@ mount --make-rslave /mnt/gentoo/sys
 mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
 
-alias c='chroot /mnt/gentoo /bin/bash && source /etc/profile && export PS1="(chroot) ${PS1}"'
+echo 'chroot /mnt/gentoo /bin/bash && source /etc/profile && export PS1="(chroot) ${PS1}"'
 
-echo "Run \"c\" to chroot"
+echo "Run above to chroot"
